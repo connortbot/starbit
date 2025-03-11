eba4d47846434e52af8e093e4cc8e54bd021c137
connortbot <loiconnor8@gmail.com>
2025-03-11 10:58:40 -0400

    control org

---
+++ client/control.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"starbit/client/game"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func (m model) HandleMenu(msg tea.Msg) (model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c":
+			if m.client != nil {
+				m.client.Close()
+			}
+			return m, tea.Quit
+		case "enter":
+			if m.username != "" && !m.started {
+				client := game.NewClient()
+				if err := client.Connect(); err != nil {
+					m.err = err
+					return m, nil
+				}
+				if err := client.SubscribeToTicks(m.username); err != nil {
+					client.Close()
+					m.err = err
+					return m, nil
+				}
+				m.client = client
+				m.tickChan = make(chan game.TickMsg, 10)
+				go handleTicks(client, m.tickChan)
+				resp, err := client.JoinGame(m.username)
+				if err != nil {
+					client.Close()
+					m.err = err
+					return m, nil
+				}
+				m.joined = true
+				m.playerCount = resp.PlayerCount
+				m.players = resp.Players
+				m.started = resp.Started
+				m.galaxy = resp.Galaxy
+				return m, waitForTicks(m.tickChan)
+			}
+		case "backspace":
+			if len(m.username) > 0 && !m.started {
+				m.username = m.username[:len(m.username)-1]
+			}
+		default:
+			if len(msg.String()) == 1 && !m.started {
+				m.username += msg.String()
+			}
+		}
+	case game.TickMsg:
+		m.playerCount = msg.PlayerCount
+		m.started = msg.Started
+		m.players = msg.Players
+		if msg.Galaxy != nil {
+			m.galaxy = msg.Galaxy
+		}
+		return m, waitForTicks(m.tickChan)
+	}
+	return m, nil
+}
+
+func (m model) HandleGame(msg tea.Msg) (model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c":
+			if m.client != nil {
+				m.client.Close()
+			}
+			return m, tea.Quit
+		case "enter":
+			debugLog.Printf("Enter: %s", m.command)
+		case "backspace":
+			if len(m.command) > 0 {
+				m.command = m.command[:len(m.command)-1]
+			}
+		default:
+			if len(msg.String()) == 1 {
+				m.command += msg.String()
+			}
+		}
+	case game.TickMsg:
+		m.playerCount = msg.PlayerCount
+		m.started = msg.Started
+		m.players = msg.Players
+		if msg.Galaxy != nil {
+			m.galaxy = msg.Galaxy
+		}
+		return m, waitForTicks(m.tickChan)
+	}
+	return m, nil
+}
